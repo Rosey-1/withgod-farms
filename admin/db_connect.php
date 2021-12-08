f6a2a57272eac9e74b200fa8bd9884859b4274aa
Rosey-1 <rosemaryanku98@gmail.com>
2021-12-08 10:15:55 -0800

    added server configuration

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,4 +1,11 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','withgod_db')or die("Could not connect to mysql".mysqli_error($con));
+
+define('SERVER', 'us-cdbr-east-04.cleardb.com');
+define('USER', 'be4a3c5be39402');
+define('DATABASE', 'heroku_7cf8a6d4a1a68ef');
+define('PASSWORD', '7b98e4e1');
+
+
+$conn= new mysqli('SERVER','USER','PASSWORD','DATABASE')or die("Could not connect to mysql".mysqli_error($con));
 
--- admin/payment/database/credentials.php
+++ admin/payment/database/credentials.php
@@ -3,9 +3,13 @@
 // set up constants for database connection
 // database, user, password, server
 
-define('SERVER', 'localhost');
-define('USER', 'root');
-define('DATABASE', 'withgod_db');
-define('PASSWORD', '');
+define('SERVER', 'us-cdbr-east-04.cleardb.com');
+define('USER', 'be4a3c5be39402');
+define('DATABASE', 'heroku_7cf8a6d4a1a68ef');
+define('PASSWORD', '7b98e4e1');
 
+
+//$cfg["Servers"][$i]["host"] = "us-cdbr-east-04.cleardb.com"; //provide hostname
+//$cfg["Servers"][$i]["user"] = "be4a3c5be39402"; //user name for your remote server//
+//$cfg["Servers"][$i]["password"] = "7b98e4e1"; //password
 ?>
